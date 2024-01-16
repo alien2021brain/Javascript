c4cedb60bdb06031c1f6a51c8efec326e82120f1
SAINATH <sachinpandey262523@gmail.com>
2024-01-16 10:13:03 +0530

    min-max

---
+++ Min-Max.js
@@ -0,0 +1,19 @@
+const arr = [10, 80, 40, 20, 60];
+function max() {
+  return arr.reduce((max, curr) => {
+    if (max < curr) {
+      max = curr;
+    }
+    return max;
+  }, 0);
+}
+function min() {
+  return arr.reduce((min, curr) => {
+    if (min > curr) {
+      min = curr;
+    }
+    return min;
+  }, arr[0]);
+}
+console.log(max());
+console.log(min());
